4f411985a49708f2a383ea419902b6325ad53f56
PLT134340 <songwoo1015@gmail.com>
2024-04-09 17:39:33 +0900

    initial commit

---
+++ src/main/java/com/canvasdiary/canvasdiaryprototype/CanvasDiaryPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.canvasdiary.canvasdiaryprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasDiaryPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanvasDiaryPrototypeApplication.class, args);
+    }
+
+}
